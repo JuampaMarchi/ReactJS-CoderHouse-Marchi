8249d580fe93e10c241a1a571ca0623bec88dc33
Juan Pablo Marchi <juanpablomarchi@gmail.com>
2021-06-13 23:00:19 -0300

    se agrego NavBar

---
+++ proyecto-juan-pablo-marchi/src/components/NavBar.js
@@ -0,0 +1,29 @@
+import { buildQueries } from '@testing-library/dom';
+import { findByLabelText } from '@testing-library/dom'
+import React from 'react'
+
+const navStyles = {
+    display: 'flex',
+    listStyleType: 'none'
+}
+const itemStyle = {
+    marginRight: '15px',
+
+}
+
+function NavBar(){
+
+    return (
+        <div style={{backgroundColor: 'blue'}}>
+            <ul style={navStyles}>
+                <li style={itemStyle}>Home</li>
+                <li style={itemStyle}>Quienes Somos</li>
+                <li style={itemStyle}>Galeria</li>
+                <li style={itemStyle}>Productos</li>
+                <li style={itemStyle}>Contacto</li>
+            </ul>
+        </div>
+    )
+}
+
+export default NavBar;
